ddefe3836451caa2605770b515ff091f1700c13d
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-29 13:13:27 -0300

    feat: Button test

---
+++ src/tests/components/Button.test.ts
@@ -0,0 +1,69 @@
+import { createRoot, Root } from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
+import { Button } from '../../components';
+
+describe('Components', () => {
+  let container: Element | null = null;
+  let root: Root | null = null;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    root = createRoot(container);
+  });
+
+  afterEach(() => act(() => root?.unmount()));
+
+  describe('<Button />', () => {
+    let button: HTMLButtonElement | null | undefined;
+
+    const onClick = jest.fn();
+
+    beforeEach(async () => {
+      act(() => root?.render(
+        Button({
+          name: 'test',
+          handleClick: onClick,
+          className: 'test',
+          type: 'button',
+        }),
+      ));
+
+      button = container?.querySelector('button');
+    });
+  
+    afterEach(() => button = null);
+
+    it('should render a button', () => {
+      expect(button).toBeTruthy();
+    });
+  
+    it('should render button\'s properties correctly', () => {
+      expect(button?.getAttribute('type')).toBe('button');
+      expect(button?.getAttribute('name')).toBe('test');
+      expect(button?.getAttribute('class')).toBe('test');
+    });
+
+    it('should render button\'s inner text correctly', () => {
+      expect(button?.textContent).toBe('test');
+    });
+
+    it('should not render a disabled and clickable button', () => {
+      expect(button).not.toBeDisabled();
+
+      act(() => button?.click());
+
+      expect(onClick).toHaveBeenCalled();
+    });
+
+    it('should render a disabled and unclickable button', () => {
+      expect(button).not.toBeDisabled();
+      button && (button.disabled = true);
+      expect(button).toBeDisabled();
+
+      act(() => button?.click());
+
+      expect(onClick).not.toHaveBeenCalled();
+    });
+  });
+});
