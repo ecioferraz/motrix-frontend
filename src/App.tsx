0fe879cdde50a27c488c716e4469181841a6eb5f
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-31 16:06:19 -0300

    feat: add Header to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Router from './routes/Router';
+import { Header } from './templates';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Router />
       </BrowserRouter>
     </div>
