4266c263374f6d2f452741060f76aae286f72e21
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-31 15:08:05 -0300

    style: reorganizing files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,13 @@
 import Button from './Button';
+import Editor from './Editor';
+import HeaderLink from './HeaderLink';
 import TextCard from './TextCard';
 import TextInput from './TextInput';
 
-export { Button, TextCard, TextInput };
+export {
+  Button,
+  Editor,
+  HeaderLink,
+  TextCard,
+  TextInput,
+};
