733027d2aa5e25c755341d67d3f26f90c5153d48
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-31 19:18:09 -0300

    refactor: BlogCard

--- src/templates/BlogCard/BlogCard.tsx
+++ src/templates/BlogCard/BlogCard.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import { TextCard } from '../../components';
+import { useNavigate } from 'react-router-dom';
+import { Button, TextCard } from '../../components';
 
 interface IBlogCard {
   body: string,
+  id: string,
   title: string,
 }
 
-export default function BlogCard({body, title}: IBlogCard) {
+export default function BlogCard({body, id, title}: IBlogCard) {
+  const navigate = useNavigate();
+
+  const handleClick = () => navigate(`/edit/${id}`);
+
   return (
-    <section
-      className='blog-post'
-    >
-      <TextCard
-        as='h1'
-        className='title'
-        text={title}
+    <div>
+      <Button
+        className='edit-btn'
+        name='edit'
+        type='button'
+        handleClick={handleClick}
       />
-      <div className='body'>
-        {body}
-      </div>
-    </section>
+      <section
+        className='blog-post'
+      >
+        <TextCard
+          as='h1'
+          className='title'
+          text={title}
+        />
+        <div className='body'>
+          {body}
+        </div>
+      </section>
+    </div>
   );
 }
