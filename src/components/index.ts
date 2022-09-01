4e82b2291b42f9f2def62ef9d94119f76b0406ca
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-01 15:07:28 -0300

    refactor: relocate Editor

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,10 @@
 import Button from './Button';
-import Editor from './Editor';
 import RedirectLink from './RedirectLink';
 import TextCard from './TextCard';
 import TextInput from './TextInput';
 
 export {
   Button,
-  Editor,
   RedirectLink,
   TextCard,
   TextInput,
