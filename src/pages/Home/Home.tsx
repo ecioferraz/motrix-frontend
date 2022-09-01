ea021dd73c84f15176af348816cf27f243ce6e58
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-01 20:31:39 -0300

    feat: blog title

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { TextCard } from '../../components';
 import { BlogPosts } from '../../templates';
+
+import './styles.css';
 
 export default function Home() {
   return (
-    <BlogPosts />
+    <main>
+      <TextCard
+        as='h1'
+        className='blog-title'
+        text='Moxxie Blog'
+      />
+      <BlogPosts />
+    </main>
   );
 }
