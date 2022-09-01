6753ae97abeb9103eb1b1bb7b67aba785a2cefce
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-01 09:11:57 -0300

    feat: Post

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,13 @@
 import EditPost from './EditPost';
 import Home from './Home';
+import Post from './Post';
+import PostHistory from './PostHistory';
 import UploadPost from './UploadPost';
 
-export { EditPost, Home, UploadPost };
+export {
+  EditPost,
+  Home,
+  Post,
+  PostHistory,
+  UploadPost,
+};
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { EditPost, Home, UploadPost } from '../pages';
+import { EditPost, Home, Post, PostHistory, UploadPost } from '../pages';
 
 export default function Router() {
   return (
@@ -10,8 +10,16 @@ export default function Router() {
         element={ <Home /> }
       />
       <Route
-        path='/post'
+        path='/:id'
+        element={ <Post /> }
+      />
+      <Route
+        path='/upload'
         element={ <UploadPost /> }
+      />
+      <Route
+        path='post/history/:id'
+        element={ <PostHistory /> }
       />
       <Route
         path='/edit/:id'
