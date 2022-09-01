af012e883b8d36c8a12ad29aed91235ed23c1454
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-01 09:11:42 -0300

    feat: PostHistory

---
+++ src/pages/PostHistory/PostHistory.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { TextCard } from '../../components';
+import { IPost } from '../../interfaces/IPost';
+import { getData } from '../../services/APIRequests';
+import { BlogCard } from '../../templates';
+
+export default function PostHistory() {
+  const { id } = useParams();
+  const [postHistory, setPostHistory] = useState<IPost[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getPostHistory = async () => {
+      const { data } = await getData(`/${id}`);
+      setPostHistory(data.history);
+      setIsLoading(false);
+    };
+
+    getPostHistory();
+  }, []);
+
+  return isLoading
+    ? <TextCard className='loading' text='Carregando...' />
+    : (
+      <>
+        {postHistory.map(({ title, updatedAt }) => (
+          <BlogCard
+            body={postHistory}
+            key={updatedAt.toString()}
+            title={title}
+          />
+        ))}
+      </>
+    );
+}
---
+++ src/pages/PostHistory/index.ts
@@ -0,0 +1,3 @@
+import PostHistory from './PostHistory';
+
+export default PostHistory;
